a0515f4eaaa0b5e8030d5eff18b83364069c5217
Ismael Gomes Moreira <ismaelmoreirakt@gmail.com>
2025-04-10 15:32:11 +0100

    feat: Addition of the program exit macro in 32-bit and 64-bit.

--- src/getArch.asm
+++ src/getArch.asm
@@ -29,3 +29,20 @@
   mov rdx, %3
   syscall
 %endmacro
+
+
+; %1 -> 32 or 64 bits
+; %2 -> exit code
+%macro exit 2
+  cmp %1, 1
+  je %%__exit
+
+  mov eax, 1
+  mov ebx, %2
+  int 0x80
+
+%%__exit:
+  mov rax, 60
+  xor rdi, %2
+  syscall
+%endmacro
